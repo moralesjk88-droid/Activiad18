14ff6180cb081d23cf468af03099ae176858cf0e
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-13 22:58:49 -0600

    ejercicio4

---
+++ EJercicio4/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Empleado
+{
+    private double salario;
+    private double salarioMinimo = 1500;
+
+    public double SAlario
+    { get
+        {
+            if (salario>=salarioMinimo)
+            {
+                return salario;   
+            }
+            else
+            {
+                Console.WriteLine("Salario Invalido");
+                return 0;
+            }
+        }
+        set { 
+            salario = value;
+        }
+    }
+
+}
