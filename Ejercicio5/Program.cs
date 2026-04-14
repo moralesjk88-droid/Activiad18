c35e1f42a5a158565700720507e2854890690ecd
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-13 23:06:58 -0600

    Ejercicio5

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Usuario
+{
+    private string contrasena;
+    
+
+    public string Contrasena
+    {
+        get
+        {
+            if (contrasena.Length >= 8)
+            {
+                return "****";
+            }
+            else
+            {
+                return "Contraseña insegura";
+            }
+        }
+        set
+        {
+            contrasena = value;
+        }
+    }
+}
