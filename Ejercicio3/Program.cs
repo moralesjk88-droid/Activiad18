14fdeeddcab1c2b557984a69127a9101f08a07f5
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-13 22:53:01 -0600

    ejercicio3

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Producto
+{
+    private double precio;
+    public double Precio
+    {
+        get
+        {
+            if (precio >= 0)
+            {
+                return precio;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        set
+        {
+            precio = value;
+        }
+    }
+}
