9e09d67002a325ff5b5d039e554d375029dcb7e9
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-13 22:39:27 -0600

    ejercicio2

---
+++ ejrcicio2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Estudiante
+{
+    private int edad;
+    
+    public int Edad
+    {
+        get
+        {
+            if (edad>=0&& edad<=100)
+            {
+                return edad;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        set
+            { edad = value; }   
+    }
+
+
+
+}
