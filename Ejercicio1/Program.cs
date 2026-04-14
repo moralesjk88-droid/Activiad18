c050907fda4a87c575c39fe203d5dae325a91f18
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-13 22:34:39 -0600

    Ejercicio1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+class CuentaBancaria
+{ 
+    private double saldo;
+
+    public double Saldo
+    { get
+        {
+            if (saldo<0)
+            {
+                return saldo;
+            }
+            else
+            {
+                Console.WriteLine("SAldo no disponnible");
+                return 0;
+            }
+
+        } 
+    set
+            {
+            saldo = value;
+        }
+    }
+
+
+
+}
